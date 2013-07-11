a24470ea613b56fc81f961aa83d2ead922efbb6a
Arun Michael Dsouza <bloggerarunmichaeldsouza@gmail.com>
2013-07-12 01:55:16 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require('fs');
+var outfile="index.html";
+fs.readFileSync("index.html",in);
+buf =new Buffer(256);
+var len;
+len = buf.write("index.html",0);
+
+    console.log(buf.toString('utf8',0,len));
+
 });
 
 var port = process.env.PORT || 5000;
